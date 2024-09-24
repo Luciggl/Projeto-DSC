c09f0e1649dc1a49fbd178fa058e453397e7a9da
Luciggl <lucielionascimento19@gmail.com>
2024-09-24 14:08:14 -0300

    chore: criando a estrutura das pastas e Configurando o Projeto

---
+++ src/test/java/com/ufpb/lucielio/nascimento/demo/ProjetoDscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpb.lucielio.nascimento.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
