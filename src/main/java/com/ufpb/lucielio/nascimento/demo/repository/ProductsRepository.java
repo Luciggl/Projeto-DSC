efa74309df06f2ce1931a5a520ff2d9b2c9694a9
Luciggl <lucielionascimento19@gmail.com>
2024-09-24 14:33:15 -0300

    feat: criando o ProductRepository

---
+++ src/main/java/com/ufpb/lucielio/nascimento/demo/repository/ProductsRepository.java
@@ -0,0 +1,6 @@
+package com.ufpb.lucielio.nascimento.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductsRepository extends JpaRepository {
+}
