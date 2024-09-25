31409ecd16e7a382490db1f845dec2c47e20b5cf
Luciggl <lucielionascimento19@gmail.com>
2024-09-25 11:10:16 -0300

    feat: criando a categoria do produto

---
+++ src/main/java/com/ufpb/lucielio/nascimento/demo/enums/CategoriaProduto.java
@@ -0,0 +1,5 @@
+package com.ufpb.lucielio.nascimento.demo.enums;
+
+public enum CategoriaProduto {
+    PADARIA,CARNES,MERCEARIA,MATINAIS,FRIOS_LATICINEOS, BEBIDAS, ULTILIDADES, LIMPEZA, HIGIENE, HORTFRUTI, PETSHOP
+}
