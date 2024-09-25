dd31c299512f219a375fc101fe0c6f462c0c66d6
Luciggl <lucielionascimento19@gmail.com>
2024-09-25 10:55:10 -0300

    chore: atualizando dependencias

--- src/main/java/com/ufpb/lucielio/nascimento/demo/repository/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ufpb.lucielio.nascimento.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository {
-}
