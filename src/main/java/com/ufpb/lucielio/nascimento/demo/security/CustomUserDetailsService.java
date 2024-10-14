2473d013c298b939e2965e49cf7818e6c0bf57b5
luciggl <lucielionascimento19@gmail.com>
2024-10-14 11:13:51 -0300

    feat: Adicionando camada de segurança

---
+++ src/main/java/com/ufpb/lucielio/nascimento/demo/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.ufpb.lucielio.nascimento.demo.security;
+
+
+import com.ufpb.lucielio.nascimento.demo.model.Usuarios;
+import com.ufpb.lucielio.nascimento.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Usuarios user = userRepository.findByLogin(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getSenha(), new ArrayList<>());
+    }
+}
